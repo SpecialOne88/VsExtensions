06784ed0c10089c7f4e1bae71b77a3d6796f7d8f
SpecialOne88 <joao.simao.1988@gmail.com>
2025-04-17 21:46:42 +0100

    Mvvm

---
+++ PowerMacros/ViewModels/MacrosWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using PowerMacros.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerMacros.ViewModels
+{
+    public class MacrosWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Macro> MacrosList { get; set; } = new ObservableCollection<Macro>();
+
+        private Macro _selectedMacro;
+        public Macro SelectedMacro
+        {
+            get { return _selectedMacro; }
+            set
+            {
+                _selectedMacro = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MacrosWindowViewModel()
+        {
+            LoadMacros();
+        }
+
+        private void LoadMacros()
+        {
+            MacrosList.Clear();
+            MacrosList.Add(new Macro { Name = "Macro1", Description = "Description1", MacroType = MacroType.Code, Code = "Code1" });
+            MacrosList.Add(new Macro { Name = "Macro2", Description = "Description2", MacroType = MacroType.Code, Code = "Code2" });
+            OnPropertyChanged(nameof(MacrosList));
+        }
+    }
+}
--- PowerMacros/Windows/MacrosWindowControl.xaml.cs
+++ PowerMacros/Windows/MacrosWindowControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using PowerMacros.Entities;
+using PowerMacros.ViewModels;
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Windows;
@@ -11,30 +12,10 @@ namespace PowerMacros.Windows
     /// </summary>
     public partial class MacrosWindowControl : UserControl
     {
-        public ObservableCollection<Macro> MacrosList { get; set; } = new ObservableCollection<Macro>();
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MacrosWindowControl"/> class.
-        /// </summary>
         public MacrosWindowControl()
         {
-            // Initialize the MacrosList with some sample data
-            MacrosList.Add(new Macro { Name = "Macro1", Description = "Description1", MacroType = MacroType.Code, Code = "Code1" });
             this.InitializeComponent();
-        }
-
-        /// <summary>
-        /// Handles click on the button by displaying a message box.
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event args.</param>
-        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
-        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(
-                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
-                "MacrosWindow");
+            DataContext = new MacrosWindowViewModel();
         }
     }
 }
